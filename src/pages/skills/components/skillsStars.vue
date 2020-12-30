1a66a4edf1eaa3552c2ef42f195d8b609d713f10
Alan <jxnx888@hotmail.com>
2020-12-30 11:21:26 +0800

    skills page

---
+++ src/pages/skills/components/skillsStars.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="skills_progress">
+      <div class="eac_skill">
+
+      </div>
+  </div>
+</template>
+
+<script>
+    export default {
+        name: "skillsProgress",
+        data() {
+            return {
+                skillsList:{
+                    "Javascript": 90,
+                    "JQuery": 90,
+                    "CSS": 90,
+                    "Three.js": 60,
+                }
+            }
+        },
+        methods: {
+
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
