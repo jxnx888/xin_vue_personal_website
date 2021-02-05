8db2988d847124df6a63ec69e8ade2e98d8cafb8
Alan <jxnx888@hotmail.com>
2021-02-05 10:59:50 +0800

    add terser-webpack-plugin to miniz/remove comment js

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,48 @@
 const webpack = require('webpack')
+const TerserPlugin = require('terser-webpack-plugin')
 module.exports = {
-    configureWebpack: {
-        plugins: [
+      configureWebpack: {
+          plugins: [
 
+              new webpack.ProvidePlugin({
+                  $: "jquery",
+                  jQuery: "jquery",
+                  jquery:"jquery",
+                  "windows.jQuery": "jquery"
+              }),
+              /*new TerserPlugin({
+                  test: /\.js(\?.*)?$/i, //用来匹配需要压缩的文件。
+                  include: /\/includes/, //匹配参与压缩的文件。
+                  exclude: /\/excludes/, //匹配不需要压缩的文件。
+                  parallel: true, // Boolean 启用/禁用多进程并发运行功能. Number 启用多进程并发运行并设置并发运行次数(parallel: 4,)。 使用多进程并发运行以提高构建速度
+                  terserOptions: { //Terser 压缩配置 。
+                      compress: {
+                          pure_funcs: ["console.log"]
+                      }
+                  }
+              })*/
+          ],
+          optimization: {
+              minimize: true,
+              minimizer: [
+                  new TerserPlugin(),
+              ],
+          },
+      }
+
+    /*configureWebpack: config => {
+        if (process.env.NODE_ENV === 'production') {
+            //生产环境配置
+            config.plugins.push(new TerserPlugin())
+            new webpack.ProvidePlugin({
+                $: "jquery",
+                jQuery: "jquery",
+                jquery:"jquery",
+                "windows.jQuery": "jquery"
+            })
+        }
+        else{
+            //开发环境配置
             new webpack.ProvidePlugin({
                 $: "jquery",
 
@@ -11,37 +51,7 @@ module.exports = {
                 "windows.jQuery": "jquery"
 
             })
+        }
+    }*/
 
-        ],
-       /* rules:[
-            // OrbitControls.js库使用
-            {
-                test: require.resolve("three/examples/js/controls/OrbitControls"),
-                use: "imports-loader?THREE=three"
-            },
-            {
-                test: require.resolve("three/examples/js/controls/OrbitControls"),
-                use: "exports-loader?THREE.OrbitControls"
-            },
-            // CSS2DRenderer.js库使用
-            {
-                test: require.resolve("three/examples/js/renderers/CSS2DRenderer.js"),
-                use: "imports-loader?THREE=three"
-            },
-            {
-                test: require.resolve("three/examples/js/renderers/CSS2DRenderer.js"),
-                use: "exports-loader?THREE.CSS2DRenderer"
-            },
-            // OBJLoader.js库使用
-            {
-                test: require.resolve("three/examples/js/loaders/OBJLoader.js"),
-                use: "imports-loader?THREE=three"
-            },
-            {
-                test: require.resolve("three/examples/js/loaders/OBJLoader.js"),
-                use: "exports-loader?THREE.OBJLoader"
-            },
-        ]*/
-
-    }
 }
