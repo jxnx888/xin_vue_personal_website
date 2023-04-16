8ebe4f50e0f353e5e232de26023b9c3afd798ade
Xin Ning <ningxin1007@hotmail.com>
2023-04-16 18:00:26 -0400

    Add open Ai to project for fun

--- server/server.js
+++ server/server.js
@@ -50,5 +50,7 @@ app.get('/', async (req, res) => {
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`App listening on port ${port}`)
 })
+
+module.exports = app;
