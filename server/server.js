f159cba9bd2a7d15b0b65f2378688b1c9de4f1d0
Xin Ning <ningxin1007@hotmail.com>
2023-04-16 18:02:46 -0400

    add serverjs

--- server/server.js
+++ server/server.js
@@ -14,7 +14,7 @@ app.use(cors({
     // (like mobile apps or curl requests)
     if (!origin) return callback(null, true);
     if (allowedOrigins.indexOf(origin) === -1) {
-      var msg = `The CORS policy for this site does not allow access from the specified Origin.`;
+      var msg = `The CORS policy for this site does not allow access from the specified Origin(${origin}).`;
       return callback(new Error(msg), false);
     }
     return callback(null, true);
